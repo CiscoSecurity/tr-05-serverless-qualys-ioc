d6117d75dfa7eb5a077c919500c7fb93962434b3
Konstantin Ogulchansky <spaceiseternal@gmail.com>
2020-04-23 15:46:02 +0300

    CCTRI-983: Got rid of `token.py`.

--- api/qualys.py
+++ api/qualys.py
@@ -1,9 +1,14 @@
 from http import HTTPStatus
+from typing import Dict
 
 import requests
-from flask import current_app
+from authlib.jose import jwt
+from flask import session, current_app, request
 
-from .token import headers
+from .url import join
+
+agent = ('Cisco Threat Response Integrations '
+         '<tr-integrations-support@cisco.com>')
 
 
 def events(filter_: str, active: bool, amount: int):
@@ -30,3 +35,43 @@ def events(filter_: str, active: bool, amount: int):
     response.raise_for_status()
 
     return response.json()
+
+
+def token(fresh: bool = False) -> str:
+    """Returns an authorization token."""
+
+    if fresh or 'token' not in session:
+        scheme, payload = request.headers['Authorization'].split(None, 1)
+
+        if scheme.lower() != 'bearer':
+            raise ValueError('Expected the scheme to be "Bearer".')
+
+        credentials = jwt.decode(payload, current_app.config['SECRET_KEY'])
+
+        username = credentials['user']
+        password = credentials['pass']
+
+        url = join(current_app.config['API_URL'], '/auth')
+        content = 'application/x-www-form-urlencoded'
+
+        response = requests.post(url,
+                                 data=b'username=' + username.encode() + b'&'
+                                      b'password=' + password.encode() + b'&'
+                                      b'token=true',
+                                 headers={'Content-Type': content,
+                                          'User-Agent': agent})
+        response.raise_for_status()
+
+        session['token'] = response.text
+
+    return session['token']
+
+
+def headers(fresh: bool = False) -> Dict[str, str]:
+    """Returns headers with an authorization token."""
+    return {
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + token(fresh),
+        'Content-Type': 'application/json',
+        'User-Agent': agent
+    }

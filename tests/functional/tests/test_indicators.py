ddeaccd905aa35a31b96dae0d2d24c1ac79ad896
mistergreen22 <44807224+mistergreen22@users.noreply.github.com>
2020-09-16 12:03:04 +0300

    Refactor tests, add test for relationships (#18)

--- tests/functional/tests/test_indicators.py
+++ tests/functional/tests/test_indicators.py
@@ -1,9 +1,12 @@
 import pytest
-
 from ctrlibrary.threatresponse.enrich import enrich_observe_observables
 from ctrlibrary.core.utils import get_observables
-
-SCHEMA_VERSION = '1.0.17'
+from tests.functional.tests.constants import (
+    MODULE_NAME,
+    CTR_ENTITIES_LIMIT,
+    SEVERITY,
+    CONFIDENCE
+)
 
 
 @pytest.mark.parametrize(
@@ -32,70 +35,30 @@ def test_positive_enrich_observe_observables_indicators(
 
     Importance: Critical
     """
-    expected_observable = {
-        'schema_version': SCHEMA_VERSION,
-        'source': 'Qualys IOC',
-        'type': 'indicator'
-    }
-
-    # Get indicator
     observables = [{"value": observable, "type": observable_type}]
-    response = enrich_observe_observables(
+    response_from_all_modules = enrich_observe_observables(
         payload=observables,
         **{'headers': module_headers}
     )['data']
-    direct_observables = get_observables(response, 'Qualys IOC')
+    response_from_qualys_ioc = get_observables(response_from_all_modules,
+                                               MODULE_NAME)
 
-    # Check respond data
-    indicator = direct_observables['data']['indicators']['docs'][0]
+    assert response_from_qualys_ioc['module']
+    assert response_from_qualys_ioc['module_instance_id']
+    assert response_from_qualys_ioc['module_type_id']
 
-    assert 'id' in indicator
-    assert 'valid_time' in indicator
+    indicators = response_from_qualys_ioc['data']['indicators']
+    assert len(indicators['docs']) > 0
 
-    for key in expected_observable.keys():
-        assert expected_observable[key] == indicator[key]
+    for indicator in indicators['docs']:
+        assert indicator['id'].startswith('transient:indicator-')
+        assert 'valid_time' in indicator
+        assert indicator['producer'] == MODULE_NAME
+        assert indicator['schema_version']
+        assert indicator['type'] == 'indicator'
+        assert indicator['source'] == MODULE_NAME
+        assert indicator['severity'] in SEVERITY
+        assert indicator['confidence'] == CONFIDENCE
+        assert indicator['external_ids']
 
-
-def test_positive_enrich_observe_observables_detail_indicators(module_headers):
-    """ Perform testing for enrich observe observables endpoint to get
-    indicators for observable Qualys module with details respond
-
-    ID: CCTRI-798-f6b973fd-29d9-40ce-96a2-491c7dda50af
-
-    Steps:
-        1. Send request with observable that has md5 type to observe
-            observables endpoint
-
-    Expectedresults:
-        1. Check that data in response body contains expected information
-            in indicators from Qualys module
-
-    Importance: Critical
-    """
-    observable = '415e5cc23e106483711abe70ad78c8e2'
-    observable_type = 'md5'
-
-    expected_observable = {
-        'schema_version': SCHEMA_VERSION,
-        'severity': 'High',
-        'source': 'Qualys IOC',
-        'producer': 'Qualys IOC',
-        'type': 'indicator',
-        'external_ids': (
-            ['F_5b49017b-90dd-4a6d-92ea-7651bafdc1ec_-8729057863409581450']),
-        'confidence': 'High'
-    }
-
-    # Get indicator
-    observables = [{"value": observable, "type": observable_type}]
-    response = enrich_observe_observables(
-        payload=observables,
-        **{'headers': module_headers}
-    )['data']
-    direct_observables = get_observables(response, 'Qualys IOC')
-
-    # Check respond data
-    indicator = direct_observables['data']['indicators']['docs'][0]
-
-    for key in expected_observable.keys():
-        assert expected_observable[key] == indicator[key]
+    assert indicators['count'] == len(indicators['docs']) <= CTR_ENTITIES_LIMIT
